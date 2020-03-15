6950dff3d3bdfba8ee85103509957df8d27f95f5
afeiship <1290657123@qq.com>
2020-03-15 13:50:24 +0800

    docs: add set random vlue

--- public/index.js
+++ public/index.js
@@ -4,14 +4,27 @@ import React from 'react';
 import './assets/style.scss';
 
 class App extends React.Component {
+  state = {
+    value: '123'
+  };
+
   onChange = (inEvent) => {
-    console.log('inEvent', inEvent.target.value);
+    // console.log('inEvent', inEvent.target.value);
+    this.setState({ value: inEvent.target.value });
+  };
+
+  setRandomValue = (e) => {
+    this.setState({ value: (Math.random() + 'value').slice(0, 5) });
   };
 
   render() {
+    const { value } = this.state;
     return (
       <div className="app-container">
-        <ReactInput defaultValue="1213" onChange={this.onChange} />
+        <button className="button" onClick={this.setRandomValue}>
+          SetRandomValue
+        </button>
+        <ReactInput value={value} onChange={this.onChange} />
       </div>
     );
   }
