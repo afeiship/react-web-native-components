f11f05de521e59a4f0fcee20f253a30ca884e914
afeiship <1290657123@qq.com>
2021-12-15 22:42:24 +0800

    test: add test commmets

--- packages/react-dialog/__tests__/index.spec.tsx
+++ packages/react-dialog/__tests__/index.spec.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import ReactDialog from '../src/main';
 
+/**
+ * 由于 dialog 的 open/close/openModal 等属性都没有在 lib.dom.d.ts 里实现
+ * 可能这个标签并非标准标签，或者有其它原因，导致 jsdom 里测试报错，暂时不用添加 test case
+ */
 describe('01/basic props', () => {
   test('<ReactDialog /> set content to body should be worked', () => {
     render(<ReactDialog>Hello Dialog</ReactDialog>);
